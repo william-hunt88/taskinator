52c85a4c5d1e4bbe62fb5ae0e2aeb338fca1de10
w.hunt <wfh2d88@gmail.com>
2021-01-09 14:05:30 -0600

    feature/drag

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,7 +206,9 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
+ 
 };
 
 var dropTaskHandler = function(event) {
@@ -225,9 +227,16 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     };
 
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
 
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if(taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+  }
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -235,3 +244,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
